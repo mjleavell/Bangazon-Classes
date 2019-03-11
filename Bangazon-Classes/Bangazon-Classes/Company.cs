ba2588717d210d152d6d5f5dad28595ea6842192
Maggie Leavell <mjleavell@gmail.com>
2019-03-10 20:52:27 -0500

    added Company class

---
+++ Bangazon-Classes/Bangazon-Classes/Company.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bangazon_Classes
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        // Create a property for holding a list of current employees
+        public List<Employee> Employees { get; set; }
+
+        public Company(string companyName, DateTime createdOn)
+        {
+            Name = companyName;
+            CreatedOn = createdOn;
+            Employees = new List<Employee>();
+        }
+
+        // Create a method that allows external code to add an employee
+        public void AddEmployee(Employee newEmployee)
+        {
+            Employees.Add(newEmployee);
+        }
+
+        // Create a method that allows external code to remove an employee
+        public void RemoveEmployee(Employee firedEmployee)
+        {
+            Employees.Remove(firedEmployee);
+        }
+
+        // Create a method to write the name of each employee to the console
+        public void ListEmployees()
+        {
+            foreach (Employee employee in Employees)
+            {
+                Console.WriteLine(employee.EmployeeName);
+            }
+        }
+    }
+}
--- Bangazon-Classes/Bangazon-Classes/Program.cs
+++ Bangazon-Classes/Bangazon-Classes/Program.cs
@@ -5,43 +5,6 @@ namespace Bangazon_Classes
 {
     class Program
     {
-        public class Company
-        {
-            public string Name { get; set; }
-            public DateTime CreatedOn { get; set; }
-
-            // Create a property for holding a list of current employees
-            public List<Employee> Employees { get; set; }
-
-            public Company(string companyName, DateTime createdOn)
-            {
-                Name = companyName;
-                CreatedOn = createdOn;
-                Employees = new List<Employee>();
-            }
-
-            // Create a method that allows external code to add an employee
-            public void AddEmployee(Employee newEmployee)
-            {
-                Employees.Add(newEmployee);
-            }
-
-            // Create a method that allows external code to remove an employee
-            public void RemoveEmployee(Employee firedEmployee)
-            {
-                Employees.Remove(firedEmployee);
-            }
-
-            // Create a method to write the name of each employee to the console
-            public void ListEmployees()
-            {
-                foreach(Employee employee in Employees)
-                {
-                    Console.WriteLine(employee.EmployeeName);
-                }
-            }
-        }
-
         // static method that launches everything
         static void Main(string[] args)
         {
